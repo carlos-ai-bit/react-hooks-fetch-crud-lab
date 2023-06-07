7e00464747fc2d0c579bb2fc8f9967dec58a64ff
Hoperaisers <abdallahsafari@gmail.com>
2023-06-07 12:52:38 +0300

    initial commit

--- src/components/AdminNavBar.js
+++ src/components/AdminNavBar.js
@@ -5,8 +5,10 @@ function AdminNavBar({ onChangePage }) {
     <nav>
       <button onClick={() => onChangePage("Form")}>New Question</button>
       <button onClick={() => onChangePage("List")}>View Questions</button>
+      <button onClick={() => onChangePage("Delete")}>Delete Question</button>
     </nav>
   );
 }
+
 
 export default AdminNavBar;
